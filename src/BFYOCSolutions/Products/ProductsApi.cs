1f91f34e5da70d11f593cda039b455ca8c413912
Christopher Morgan <ddi6174@hca.corpad.net>
2020-11-18 15:18:50 -0600

    didn't save productsAPI

--- src/BFYOCSolutions/Products/ProductsApi.cs
+++ src/BFYOCSolutions/Products/ProductsApi.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BFYOCSolutions.Products
 {
     class ProductsApi
     {
+        private const string BaseUrl = "https://serverlessohuser.trafficmanager.net/api/";
+
+        public static async Task<Product> GetProductByIdAsync(Guid productId)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(BaseUrl);
+                var response = await httpClient.GetStringAsync($"GetProduct?productId={productId}");
+                var user = JsonConvert.DeserializeObject<Product>(response);
+                return user;
+            }
+        }
     }
 }
